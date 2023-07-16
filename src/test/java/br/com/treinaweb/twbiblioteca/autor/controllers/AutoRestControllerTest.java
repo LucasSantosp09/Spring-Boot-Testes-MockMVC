c2db366e79f6f106a5e3e676b1ece07a891d28b3
LucasSantosp09 <lucassantos.p09@gmail.com>
2023-07-16 12:24:44 -0300

    Testando action buscar Todos

--- src/test/java/br/com/treinaweb/twbiblioteca/autor/controllers/AutoRestControllerTest.java
+++ src/test/java/br/com/treinaweb/twbiblioteca/autor/controllers/AutoRestControllerTest.java
@@ -1,10 +1,21 @@
 package br.com.treinaweb.twbiblioteca.autor.controllers;
 
+
+import br.com.treinaweb.twbiblioteca.autor.builders.AutorResponseBuilder;
 import br.com.treinaweb.twbiblioteca.autor.services.AutorService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+
+
 
 @WebMvcTest(AutorRestController.class)
 public class AutoRestControllerTest {
@@ -15,4 +26,15 @@ public class AutoRestControllerTest {
     private MockMvc mockMvc;
 
     private static final String AUTOR_API_URL_PATH = "/api/v1/autores";
-}
+
+    @Test
+    void quandoGETBuscarTodosDeveRetornarListaDeAutoresComStatusCode200 () throws Exception {
+        var responseBody = AutorResponseBuilder.builder().build();
+
+        Mockito.when(service.buscarTodos()).thenReturn(List.of(responseBody));
+
+        mockMvc.perform(MockMvcRequestBuilders.get(AUTOR_API_URL_PATH).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+    }
+
